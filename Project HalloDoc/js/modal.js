89096070b28a2740c1f459ee828c8d4166689c04
Karmadipsinh Solanki <PCA161@server7.com>
2024-01-19 18:40:14 +0530

    completed 4 patient request form pages

---
+++ Project HalloDoc/js/modal.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    // Code to execute after the entire page loads
+    <div class="modal-dialog modal-dialog-centered">
+       
+
+    
+
+
+    </div>
+    alert("The page has loaded!");
+}
