e4383007cf1a447cce586311870cf489f0d85195
Karmadipsinh Solanki <PCA161@server7.com>
2024-02-05 19:25:55 +0530

    converted all html css js pages to ASP.NET MVC Core 7

---
+++ Hallodoc/Hallodoc/Controllers/PatientRequestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hallodoc.Controllers
+{
+    public class PatientRequestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult createPatientRequest()
+        {
+            return View();
+        }
+        public IActionResult createFamilyRequest()
+        {
+            return View();
+        }
+        public IActionResult createConciergeRequest()
+        {
+            return View();
+        }
+        public IActionResult createBusinessRequest()
+        {
+            return View();
+        }
+    }
+}
