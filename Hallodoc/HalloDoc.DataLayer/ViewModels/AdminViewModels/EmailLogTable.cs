4549741842f052d3e39ee8d265076b38ff51aa4a
Karmadipsinh Solanki <PCA161@server7.com>
2024-04-29 18:26:46 +0530

    final

--- Hallodoc/HalloDoc.DataLayer/ViewModels/AdminViewModels/EmailLogTable.cs
+++ Hallodoc/HalloDoc.DataLayer/ViewModels/AdminViewModels/EmailLogTable.cs
@@ -8,20 +8,20 @@ namespace HalloDoc.DataLayer.ViewModels.AdminViewModels
 {
     public class EmailLogTable
     {
-        public int EmailLogId { get; set; }
+        public int? EmailLogId { get; set; }
 
         public string? Recipient { get; set; }
 
         public string? Action { get; set; }
 
-        public int RoleName { get; set; }
+        public int? RoleName { get; set; }
 
         public string? Email { get; set; }
 
-        public DateTime CreatedDate { get; set; }
-        public DateTime SentDate { get; set; }
-        public string Sent { get; set; }
-        public int SentTries { get; set; }
-        public string ConfirmationNumber { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? SentDate { get; set; }
+        public string? Sent { get; set; }
+        public int? SentTries { get; set; }
+        public string? ConfirmationNumber { get; set; }
     }
 }
